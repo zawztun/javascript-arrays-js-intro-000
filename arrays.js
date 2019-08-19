01d8f8efa2518da4e407cde85b40ad7bd71ea6e7
Donal tun <americanarakan@gmail.com>
2019-08-19 03:21:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -48,6 +48,6 @@ function removeElementFromBeginningOfArray(array){
 
 function removeElementFromBeginningOfArray(array){
   let items = [1, 2, 3]
-  items.splice(1,1)
+  items.splice(0,1)
   return array
 }
